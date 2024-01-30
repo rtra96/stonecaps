0e7e6773ce8acd26d4c880f270235202d7b14086
Rick Trahant <rtra96@gmail.com>
2024-01-29 18:15:38 -0600

    re-do for last committ..forgot to save-all :(

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,36 +1,3 @@
-// import React, { createContext, useContext, useState } from 'react';
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null); // Initially, no user is authenticated
-
-//   const login = (userData) => {
-//     setUser(userData);
-//   };
-
-//   const logout = () => {
-//     localStorage.removeItem("token");
-//     localStorage.removeItem("userInfo");
-//     setUser(null);
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ user, login, logout, setUser }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => {
-//   const context = useContext(AuthContext);
-//   console.log('Auth Context:', context);
-//   if (!context) {
-//     throw new Error('useAuth must be used within an AuthProvider');
-//   }
-//   return context;
-// };
-
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
